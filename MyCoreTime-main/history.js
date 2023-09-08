a6edd68ab3171e8481ec721ee2dbfe0b9474fe31
Zaid Sheikh <Zaidsh02@gmail.com>
2023-09-09 01:10:16 +0200

    First prototype

---
+++ MyCoreTime-main/history.js
@@ -0,0 +1,55 @@
+// history.js
+
+// Function to load and display logged times
+function loadTimeHistory() {
+    // You can fetch data from a text file or another source here.
+
+  
+    const historyContainer = document.getElementById("historycontainer");
+    historyContainer.innerHTML = "";
+  
+    exampleTimes.forEach((log) => {
+      const logElement = document.createElement("p");
+      logElement.textContent = log;
+      historyContainer.appendChild(logElement);
+    });
+  }
+  
+  // Load and display logged times when the history page is loaded
+  window.addEventListener("load", loadTimeHistory);
+  
+  // Back to Main button click event
+  document.getElementById("backtomain").addEventListener("click", () => {
+    // Redirect back to the main page (index.html) or use your navigation logic.
+    window.location.href = "index.html";
+  });
+
+  function clearTimeHistory() {
+    // Clear the history in local storage
+    localStorage.removeItem("timeHistory");
+  
+    // Refresh the displayed history
+    loadTimeHistory();
+  }
+  
+  // Add an event listener for the "Clear History" button
+  document.getElementById("clearhistory").addEventListener("click", clearTimeHistory);
+  
+  
+  // Function to load and display logged times from local storage
+  function loadTimeHistory() {
+    const historyContainer = document.getElementById("historycontainer");
+    historyContainer.innerHTML = "";
+  
+    // Retrieve the history from local storage
+    const timeHistory = JSON.parse(localStorage.getItem("timeHistory")) || [];
+  
+    timeHistory.forEach((log) => {
+      const logElement = document.createElement("p");
+      logElement.textContent = log;
+      historyContainer.appendChild(logElement);
+    });
+  }
+  
+  
+
---
+++ MyCoreTime-main/popup.js
@@ -0,0 +1,111 @@
+let timerInterval;
+let startTime = 0;
+let elapsedTime = 0;
+
+let isPaused = false;
+let pauseStartTime = 0;
+let totalPauseTime = 0;
+
+
+function formatTime(milliseconds) {
+  const seconds = Math.floor((milliseconds / 1000) % 60);
+  const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+  const hours = Math.floor(milliseconds / 1000 / 60 / 60);
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`; //format for 00:00:00
+}
+
+function updateTimer() {
+  const currentTime = Date.now();
+  if (!isPaused) {
+    elapsedTime = currentTime - startTime - totalPauseTime;
+    document.getElementById("timer").innerText = formatTime(elapsedTime);
+  }
+}
+
+//Start button
+document.getElementById("start").addEventListener("click", () => {
+  if (!timerInterval) {
+    if (isPaused) {
+      totalPauseTime += Date.now() - pauseStartTime;
+    }
+    startTime = Date.now() - elapsedTime - totalPauseTime;
+    timerInterval = setInterval(updateTimer, 1000); // Update every second (1000ms).
+  }
+  isPaused = false;
+});
+
+//Paused button
+document.getElementById("pause").addEventListener("click", () => {
+  if (!isPaused && timerInterval) {
+    isPaused = true;
+    pauseStartTime = Date.now(); // Store the time when pause was pressed.
+    clearInterval(timerInterval); // Stop the timer.
+    timerInterval = null;
+  }
+});
+
+//Stop button
+document.getElementById("stop").addEventListener("click", () => {
+  if (timerInterval) {
+    clearInterval(timerInterval); // Clear the interval to stop updating the timer.
+    timerInterval = null;
+  }
+  const logTime = confirm("Do you want to log this time in History?");
+  if (logTime) {
+    const formattedTime = formatTime(elapsedTime);
+    saveTimeLog(`Time logged: ${formattedTime}`);
+  }
+  resetTimer();
+});
+
+//Reset timer when pressed Stop
+function resetTimer() {
+  elapsedTime = 0;
+  totalPauseTime = 0;
+  document.getElementById("timer").innerText = "00:00:00"; // Resets the timer to 00:00:00
+}
+
+//Used for saving the time as a tx.t
+/*function saveTimeLog(log) {
+  const blob = new Blob([log], { type: "text/plain"});
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "LoggedTime.txt";
+  a.click();
+
+  URL.revokeObjectURL(url);
+}*/
+
+// Function to open the history page
+function openHistoryPage() {
+    window.location.href = "history.html";
+  }
+  
+  // Add an event listener for the "View History" button
+  document.getElementById("viewhistory").addEventListener("click", openHistoryPage);
+  
+
+  // In popup.js
+
+// Function to save the log to local storage
+function saveTimeLog(log) {
+    const blob = new Blob([log], { type: "text/plain"});
+    const url = URL.createObjectURL(blob);
+    // Get the existing history or initialize an empty array
+    const existingHistory = JSON.parse(localStorage.getItem("timeHistory")) || [];
+    
+    // Add the new log to the history
+    // Add the new log with date and time
+  const now = new Date();
+  const formattedDateTime =`Time: ${now.toLocaleTimeString()}, Date: ${now.toLocaleDateString()}`;
+  const logWithDateTime = `${log}, ${formattedDateTime}`;
+  
+  existingHistory.push(logWithDateTime);
+    //existingHistory.push(log);
+    
+    // Store the updated history in local storage
+    localStorage.setItem("timeHistory", JSON.stringify(existingHistory));
+  }
+  
