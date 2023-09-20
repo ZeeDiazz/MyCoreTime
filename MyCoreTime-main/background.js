bea7c2310490a3fd4ef249fffbe49e7df847da22
Zaid Sheikh <Zaidsh02@gmail.com>
2023-09-21 00:38:47 +0200

    Comment saved when popup closes

--- MyCoreTime-main/background.js
+++ MyCoreTime-main/background.js
@@ -7,95 +7,103 @@ let isPaused = false;
 let pauseStartTime = 0;
 let totalPauseTime = 0;
 
+//let alarmInterval;
+
 function formatTime(milliseconds) {
-  const seconds = Math.floor((milliseconds / 1000) % 60);
-  const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
-  const hours = Math.floor(milliseconds / 1000 / 60 / 60);
-  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`; //format for 00:00:00
+    const seconds = Math.floor((milliseconds / 1000) % 60);
+    const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+    const hours = Math.floor(milliseconds / 1000 / 60 / 60);
+    return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`; //format for 00:00:00
 }
+
+chrome.runtime.onMessage.addListener(function (message) {
+    if (message.type === 'saveThisComment') {
+        const commentbg = message.comment;
+        chrome.storage.local.set({ comment: commentbg });
+    }
+});
 function resetTimer() {
-  elapsedTime = 0;
-  totalPauseTime = 0;
-  document.getElementById("timer").innerText = "00:00:00"; // Resets the timer to 00:00:00
+    elapsedTime = 0;
+    totalPauseTime = 0;
+    document.getElementById("timer").innerText = "00:00:00"; // Resets the timer to 00:00:00
 }
 
 function updateTimer() {
-  const currentTime = Date.now();
-  if (!isPaused) {
-    elapsedTime = currentTime - startTime;
-    const formattedTime = formatTime(elapsedTime);
+    const currentTime = Date.now();
+    if (!isPaused) {
+        elapsedTime = currentTime - startTime;
+        const formattedTime = formatTime(elapsedTime);
 
-    // Send the updated time to all popup instances
-    chrome.runtime.sendMessage({ type: 'updateTime', time: formattedTime });
-  }
+        // Send the updated time to all popup instances
+        chrome.runtime.sendMessage({ type: 'updateTime', time: formattedTime });
+    }
 }
 
 function startTimer() {
-  if (!timerInterval) {
-    if (isPaused) {
-      totalPauseTime += Date.now() - pauseStartTime;
-      startTime += Date.now() - pauseStartTime;
-    } else {
-      startTime = Date.now() - elapsedTime - totalPauseTime;
+    if (!timerInterval) {
+        if (isPaused) {
+            totalPauseTime += Date.now() - pauseStartTime;
+            startTime += Date.now() - pauseStartTime;
+        } else {
+            startTime = Date.now() - elapsedTime - totalPauseTime;
+        }
+        timerInterval = setInterval(updateTimer, 1000); // Update every second.
     }
-    timerInterval = setInterval(updateTimer, 1000); // Update every second.
-  }
-  isPaused = false;
+    isPaused = false;
 }
 
 function pauseTimer() {
-  if (!isPaused && timerInterval) {
-    isPaused = true;
-    pauseStartTime = Date.now(); // Store the time when paused.
-    clearInterval(timerInterval); // Stop the timer.
-    timerInterval = null;
-  }
+    if (!isPaused && timerInterval) {
+        isPaused = true;
+        pauseStartTime = Date.now(); // Store the time when paused.
+        clearInterval(timerInterval); // Stop the timer.
+        timerInterval = null;
+    }
 }
 
 function stopTimer() {
-  if (timerInterval) {
-    clearInterval(timerInterval);
-    timerInterval = null;
-  }
+    if (timerInterval) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+    }
 }
 
 // Listen for messages from popup.js
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type === 'startTimer') {
-    startTimer();
-  } else if (message.type === 'pauseTimer') {
-    pauseTimer();
-  } else if (message.type === 'stopTimer') {
-    stopTimer();
-  } else if (message.type === 'viewHis') {
-    window.location.href = "history.html";
-  } else if (message.type === 'sendLoggedTime') {
-    wantToLogTime();
-  } else if (message.type === 'resetTimerNow') {
-    resetTimer();
-  }
+    if (message.type === 'startTimer') {
+        startTimer();
+    } else if (message.type === 'pauseTimer') {
+        pauseTimer();
+    } else if (message.type === 'stopTimer') {
+        stopTimer();
+    } else if (message.type === 'viewHis') {
+        window.location.href = "history.html";
+    } else if (message.type === 'sendLoggedTime') {
+        wantToLogTime();
+    } else if (message.type === 'resetTimerNow') {
+        resetTimer();
+    }
 });
 
 function wantToLogTime() {
-  let wantToLog = "";
+    let wantToLog = "";
 
-  const formattedTime = formatTime(elapsedTime);
+    const formattedTime = formatTime(elapsedTime);
 
-  if (formattedTime === '00:00:00') {
-    wantToLog = "Can't log 00:00:00";
-  } else {
-    wantToLog = "Do you want to log?";
-  }
-  chrome.runtime.sendMessage({ type: 'loggedTime', time: formattedTime, confirm: wantToLog });
+    if (formattedTime === '00:00:00') {
+        wantToLog = "Can't log 00:00:00";
+    } else {
+        wantToLog = "Do you want to log?";
+    }
+    chrome.runtime.sendMessage({ type: 'loggedTime', time: formattedTime, confirm: wantToLog });
 }
 
 // When a popup is opened, send the current time to it
 chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function (message) {
-    if (message.type === 'requestTime' && !isPaused) {
-      const formattedTime = formatTime(elapsedTime);
-      port.postMessage({ type: 'updateTime', time: formattedTime });
-    }
-  });
+    port.onMessage.addListener(function (message) {
+        if (message.type === 'requestTime' && !isPaused) {
+            const formattedTime = formatTime(elapsedTime);
+            port.postMessage({ type: 'updateTime', time: formattedTime });
+        }
+    });
 });
-
--- MyCoreTime-main/popup.js
+++ MyCoreTime-main/popup.js
@@ -4,77 +4,114 @@ port.postMessage({ type: 'requestTime' });
 
 // Initialize background page
 function formatTime(milliseconds) {
-  const seconds = Math.floor((milliseconds / 1000) % 60);
-  const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
-  const hours = Math.floor(milliseconds / 1000 / 60 / 60);
-  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`; //format for 00:00:00
+    const seconds = Math.floor((milliseconds / 1000) % 60);
+    const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+    const hours = Math.floor(milliseconds / 1000 / 60 / 60);
+    return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`; //format for 00:00:00
 }
 
 // Add an event listener to receive updated time from background.js
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.type === 'updateTime') {
-    document.getElementById("timer").innerText = message.time;
-  }
+    if (message.type === 'updateTime') {
+        document.getElementById("timer").innerText = message.time;
+    }
 });
 
 // Start button
 document.getElementById("start").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ type: 'startTimer' });
+    chrome.runtime.sendMessage({ type: 'startTimer' });
 });
 
 // Pause button
 document.getElementById("pause").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ type: 'pauseTimer' });
+    chrome.runtime.sendMessage({ type: 'pauseTimer' });
 });
 
 // Stop button
 document.getElementById("stop").addEventListener("click", () => {
-
-  chrome.runtime.sendMessage({ type: 'stopTimer' });
-  chrome.runtime.sendMessage({ type: 'sendLoggedTime' });
+    chrome.runtime.sendMessage({ type: 'stopTimer' });
+    chrome.runtime.sendMessage({ type: 'sendLoggedTime' });
 });
 
-chrome.runtime.onMessage.addListener(function (message) {
-  if (message.type === 'loggedTime' && message.time != '00:00:00') {
-    const logTime = confirm(message.confirm);
-    if (logTime) {
-      saveTimeLog(`${message.time}`);
-      resetTimer();
-      chrome.runtime.sendMessage({ type: 'resetTimerNow' });
+const comment = document.getElementById("comment");
+//Save the comment whenever there is a input
+comment.addEventListener("input", () => {
+    let saveCommentValue = comment.value;
+    chrome.storage.local.set({ comment: saveCommentValue });
+});
+
+// when the popup opens do this
+chrome.storage.local.get(["comment"], function (result) {
+    if (result.comment) {
+        comment.value = result.comment;
     }
-  } else if (message.type === 'loggedTime') {
-    alert(message.confirm);
-  }
+});
+
+/*document.getElementById("alarm").addEventListener("click", () => {
+    const alarmBox = document.getElementById("alarmBox");
+    alarmBox.style.display = "block";
+
+    const setAlarm = document.getElementById("setAlarm");
+    setAlarm.addEventListener("click", () => {
+        //ALARM HER
+        let sec = document.getElementById("alarmTime").value;
+        formatTime(sec);
+
+
+        // make it invensible
+        alarmBox.style.display = "none";
+    });
+
+    const cancelAlarm = document.getElementById("cancelAlarm");
+    cancelAlarm.addEventListener("click", () => {
+        // make it invensible
+        alarmBox.style.display = "none";
+    });
+});*/
+chrome.runtime.onMessage.addListener(function (message) {
+    if (message.type === 'loggedTime' && message.time != '00:00:00') {
+        const logTime = confirm(message.confirm);
+        if (logTime) {
+            saveTimeLog(`${message.time}`);
+            resetTimer();
+            chrome.runtime.sendMessage({ type: 'resetTimerNow' });
+        }
+    } else if (message.type === 'loggedTime') {
+        alert(message.confirm);
+    }
 });
 
 document.getElementById("viewhistory").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ type: 'viewHis' });
-  window.location.href = "history.html";
+    chrome.runtime.sendMessage({ type: 'viewHis' });
+    window.location.href = "history.html";
 });
 
 function resetTimer() {
-  elapsedTime = 0;
-  totalPauseTime = 0;
-  document.getElementById("timer").innerText = "00:00:00"; // Resets the timer to 00:00:00
+    elapsedTime = 0;
+    totalPauseTime = 0;
+    document.getElementById("timer").innerText = "00:00:00"; // Resets the timer to 00:00:00
 }
 
 function saveTimeLog(log) {
-  const blob = new Blob([log], { type: "text/plain" });
-  const url = URL.createObjectURL(blob);
-  // Get the existing history or initialize an empty array
-  const existingHistory = JSON.parse(localStorage.getItem("timeHistory")) || [];
+    const blob = new Blob([log], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    // Get the existing history or initialize an empty array
+    const existingHistory = JSON.parse(localStorage.getItem("timeHistory")) || [];
 
-  // Add the new log with date and time
-  const now = new Date();
-  const test = document.getElementById("comment").value;
+    // Add the new log with date and time
+    const now = new Date();
+    const saveComment = comment.value;
+    const testNoComma = saveComment.replace(',', '--');
 
-  const testNoComma = test.replace(',', '--');
+    //reset the comment
+    comment.value = '';
+    chrome.storage.local.set({ comment: '' });
 
-  const formattedDateTime = `${now.toLocaleTimeString()}, ${now.toLocaleDateString()}, ${testNoComma}`;
-  const logWithDateTime = `${log}, ${formattedDateTime}`; //Format Timelog, TimeOfLog, Date
+    const formattedDateTime = `${now.toLocaleTimeString()}, ${now.toLocaleDateString()}, ${testNoComma}`;
+    const logWithDateTime = `${log}, ${formattedDateTime}`; //Format Timelog, TimeOfLog, Date
 
-  existingHistory.push(logWithDateTime);
+    existingHistory.push(logWithDateTime);
 
-  // Store the updated history in local storage
-  localStorage.setItem("timeHistory", JSON.stringify(existingHistory));
+    // Store the updated history in local storage
+    localStorage.setItem("timeHistory", JSON.stringify(existingHistory));
 }
