b41373607ab3b0dfe66d1d3a5907831f0b8c63eb
Zaid Sheikh <Zaidsh02@gmail.com>
2023-09-25 22:52:17 +0200

    Download selected files

--- MyCoreTime-main/history.js
+++ MyCoreTime-main/history.js
@@ -1,3 +1,6 @@
+const a = document.createElement("a");
+let selectedLogs = [];
+
 function loadTimeHistory() {
     const historyContainer = document.getElementById("historycontainer");
     historyContainer.innerHTML = "";
@@ -47,27 +50,59 @@ function loadTimeHistory() {
         const cell4 = row.insertCell(3);
         const cell5 = row.insertCell(4);
         const cell6 = row.insertCell(5);
+        //const cell7 = row.insertCell(6);
 
-        cell1.textContent = loggedTime;
-        cell2.textContent = timeOfLogged;
-        cell3.textContent = loggedDate;
-        cell4.textContent = comment;
+        // Add a checkbox to the first cell
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.addEventListener("change", () => updateSelectedLogs());
+        cell1.appendChild(checkbox);
+
+
+        cell2.textContent = loggedTime;
+        cell3.textContent = timeOfLogged;
+        cell4.textContent = loggedDate;
+        cell5.textContent = comment;
 
         //edit log coloum 
         const editButton = document.createElement("img");
         editButton.src = "/images/editIcon25.png";
         editButton.classList.add("editButton"); //to make the hover action
         editButton.addEventListener("click", () => editLog(index));
-        cell5.appendChild(editButton);
+        cell6.appendChild(editButton);
 
-        const downloadButton = document.createElement("img");
+        /*const downloadButton = document.createElement("img");
         downloadButton.src = "/images/downloadIcon25.png";
         downloadButton.classList.add("downloadButton");
         downloadButton.addEventListener("click", () => downloadLog(log)); // Call the download function
-        cell6.appendChild(downloadButton);
+        cell7.appendChild(downloadButton);*/
 
     });
 }
+
+function updateSelectedLogs() {
+    selectedLogs = [];
+    const checkboxes = document.querySelectorAll("#historytable tbody input[type='checkbox']");
+    checkboxes.forEach((checkbox, index) => {
+        if (checkbox.checked) {
+            selectedLogs.push(index);
+        }
+    });
+}
+
+document.getElementById("downloadAll").addEventListener("click", () => {
+    const selectedLogData = selectedLogs.map((index) => {
+        return JSON.parse(localStorage.getItem("timeHistory"))[index];
+    });
+    if (selectedLogData.length > 0) {
+        const combinedLog = selectedLogData.join("\n");
+        downloadLog(combinedLog);
+    } else {
+        alert("No logs selected for download.");
+    }
+    a.clearTimeHistory();
+});
+
 
 function editLog(index) {
     const logIndex = index;
@@ -127,28 +162,42 @@ function editLog(index) {
     });
 
 }
+
+document.getElementById("fileType").addEventListener("change", () => {
+    fileType = document.getElementById("fileType").value;
+});
 // Function to trigger the download action
 function downloadLog(log) {
     const selectForm = document.getElementById("selectBox");
     selectForm.style.display = "block";
 
-    let fileType = document.getElementById("fileType").value;
-
     const downloadButton = document.getElementById("downloadConfirm");
-    downloadButton.addEventListener("click", () => {
 
-        const filename = "LoggedTime." + fileType;
-        const blob = new Blob([log], { type: "text/plain" });
+    // Remove previous event listeners
+    const newDownloadButton = downloadButton.cloneNode(true);
+    downloadButton.parentNode.replaceChild(newDownloadButton, downloadButton);
 
-        const a = document.createElement("a");
-        a.href = URL.createObjectURL(blob);
-        a.download = filename;
+    newDownloadButton.addEventListener("click", () => {
+        if (fileType == 'None') {
+            alert('Nope');
+        } else {
+            let filename = "LoggedTimeDDDD" + fileType;
+            const blob = new Blob([log], { type: "text/plain" });
 
-        a.click();
-        selectForm.style.display = "none";
+            a.href = URL.createObjectURL(blob);
+            a.download = filename;
+
+            a.click();
+            selectForm.style.display = "none";
+
+            const checkboxes = document.querySelectorAll("#historytable tbody input[type='checkbox']");
+            checkboxes.forEach((checkbox) => {
+                checkbox.checked = false;
+            });
+        }
     });
-     // Cancel button
-    const cancelDownloadButton = document.getElementById("cancelDownload");
+    // Cancel button
+    const cancelDownloadButton = document.getElementById("downloadCancel");
     cancelDownloadButton.addEventListener("click", () => {
         // make it invensible
         selectForm.style.display = "none";
